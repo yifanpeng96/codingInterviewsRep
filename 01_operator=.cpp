0969697be2613c8ac36f3a81cd460bf1aead47b8
yifanpeng96 <yifan_peng@foxmail.com>
2021-09-17 16:37:12 +0800

    Update 01_operator=.cpp

--- 01_operator=.cpp
+++ 01_operator=.cpp
@@ -6,22 +6,21 @@ class CMyString {
 public:
 	CMyString(char* pData = nullptr);
 	CMyString(const CMyString& str);
-	~CMyString(void);
+	~CMyString();
 
 private:
 	char* m_pData;
 };
 
-//赋值运算符函数
-//把返回值类型声明为该类型的引用 CMyString& , 因为这样才可以连续赋值
-//传入参数应声明为常量引用 const CMyString& , 若不是则从形参到实参会调用一次拷贝构造函数
+//把返回值类型声明为应用, 并return自身的引用(*this), 才可以连续赋值
+//把传入的参数声明为常量引用, 若不是则从形参到实参会调用一次拷贝构造函数
 CMyString& CMyString::operator=(const CMyString& str) {
-	//若不判断传入参数和当前实例是否为同一实例, 在释放实例自身内存时会出现严重问题
-	if(this == &str)
-		return *this;//this指代 CMyString 的一个对象的指针
+	//判断传入的参数和当前实例(*this)是不是同一个实例. 如果是, 在释放自身内存时会导致严重问题
+	if (this == &str)
+		return *this; //this指代 CMyString 的一个对象的指针
 
-	//先释放实例自身已有的内存, 不然会出现内存泄漏
-	delete[]m_pData;
+	//先释放实例自身已有的内存, 否则会出现内存泄露
+	delete[] m_pData;
 	m_pData = nullptr;
 
 	m_pData = new char[strlen(str.m_pData) + 1];
